fee28484780725c0c3cb1c8e702b6bcfa215ad1a
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-03-15 01:26:01 +0530

    Fix timer code

--- app/src/main/java/com/iam2kabhishek/evaltrivia/TriviaActivity.kt
+++ app/src/main/java/com/iam2kabhishek/evaltrivia/TriviaActivity.kt
@@ -11,14 +11,16 @@ class TriviaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trivia)
+        startTimeCounter(window.decorView.rootView)
     }
 
-    fun startTimeCounter(view: View) {
+    private fun startTimeCounter(view: View) {
         val timerText = findViewById<TextView>(R.id.timer_text)
         object : CountDownTimer(49999, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 timerText.text = timerCount.toString()
-                timerCount++
+                timerCount--
+
             }
             override fun onFinish() {
                 timerText.text = "Game Over!"
