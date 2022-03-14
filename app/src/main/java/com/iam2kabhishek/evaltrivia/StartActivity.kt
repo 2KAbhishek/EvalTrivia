3e9582f3fe9e0189385a79ce764319b8fc00265d
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-03-14 19:26:27 +0530

    Add transtion to about on click

--- app/src/main/java/com/iam2kabhishek/evaltrivia/StartActivity.kt
+++ app/src/main/java/com/iam2kabhishek/evaltrivia/StartActivity.kt
@@ -1,11 +1,18 @@
 package com.iam2kabhishek.evaltrivia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+        val aboutButton = findViewById<Button>(R.id.button_about)
+        aboutButton.setOnClickListener {
+            val intent = Intent(this, AboutActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
