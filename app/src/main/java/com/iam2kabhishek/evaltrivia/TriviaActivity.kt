b0e87b6813c88eff9044cf13582ee07b80df23b6
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-03-15 01:14:39 +0530

    Add timer to trivia activity class

--- app/src/main/java/com/iam2kabhishek/evaltrivia/TriviaActivity.kt
+++ app/src/main/java/com/iam2kabhishek/evaltrivia/TriviaActivity.kt
@@ -2,10 +2,27 @@ package com.iam2kabhishek.evaltrivia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.TextView
 
 class TriviaActivity : AppCompatActivity() {
+    var timerCount = 50
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trivia)
+    }
+
+    fun startTimeCounter(view: View) {
+        val timerText = findViewById<TextView>(R.id.timer_text)
+        object : CountDownTimer(49999, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                timerText.text = timerCount.toString()
+                timerCount++
+            }
+            override fun onFinish() {
+                timerText.text = "Game Over!"
+            }
+        }.start()
     }
 }
