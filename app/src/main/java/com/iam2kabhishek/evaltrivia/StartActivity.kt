559a2a5c6481bad0a25da7e223e252ef52e4c100
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-03-14 18:22:54 +0530

    Add empty start activity code

---
+++ app/src/main/java/com/iam2kabhishek/evaltrivia/StartActivity.kt
@@ -0,0 +1,11 @@
+package com.iam2kabhishek.evaltrivia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
