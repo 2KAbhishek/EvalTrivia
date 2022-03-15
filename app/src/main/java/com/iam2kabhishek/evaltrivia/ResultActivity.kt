bc0154984d1952e499366fec2f4cd34cecde32ac
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-03-15 11:37:26 +0530

    Get extras and show data

--- app/src/main/java/com/iam2kabhishek/evaltrivia/ResultActivity.kt
+++ app/src/main/java/com/iam2kabhishek/evaltrivia/ResultActivity.kt
@@ -2,10 +2,16 @@ package com.iam2kabhishek.evaltrivia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        val correct = intent.getIntExtra("CORRECT", 1)
+        val incorrect = intent.getIntExtra("INCORRECT", 1)
+
+        val resultText = findViewById<TextView>(R.id.result_text)
+        "You gave $correct correct and $incorrect incorrect answers".also { resultText.text = it }
     }
 }
